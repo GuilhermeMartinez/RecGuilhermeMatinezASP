8afdeaa69979c13738a3620cad0be39071840e4e
GuilhermeMartinez <guigui.martinez20@hotmail.com>
2018-05-04 22:25:05 -0300

    Alteração

---
+++ RecGuilhermeMASP/Models/DataAccessObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RecGuilhermeMASP.Models
+{/// <summary>
+/// Chamada do Banco Modelo para Tratar (DAO)
+/// </summary>
+    public class DataAccessObject:DbContext
+    {
+        public DataAccessObject() : base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<RecGuilhermeMASP.Models.Category> Categories { get; set; }
+
+        public System.Data.Entity.DbSet<RecGuilhermeMASP.Models.News> News { get; set; }
+
+    }
+}
